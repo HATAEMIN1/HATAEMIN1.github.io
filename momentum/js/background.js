36d491da2e25b83087ed7d9ee1d16f9c2da1da4f
HATAEMIN1 <gkxoals33@gmail.com>
2022-10-09 23:44:13 +0900

    first
    first

---
+++ momentum/js/background.js
@@ -0,0 +1,9 @@
+const images = ["3.png", "4.png", "5.png", "6.png"];
+
+const chosenImage = images[Math.floor(Math.random() * images.length)];
+
+const bgImage = document.createElement("img");
+
+bgImage.src = `img/${chosenImage}`;
+
+document.body.appendChild(bgImage);
